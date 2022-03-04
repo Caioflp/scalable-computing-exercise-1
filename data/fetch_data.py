1d6c632e396954930547619fdbd3536bf56349a6
jvprimakipr <j.joaoviniciuspr@hotmail.com>
2022-03-04 16:45:16 -0300

    Correction

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -5,14 +5,15 @@ if not os.path.exists("./shakespeare.txt"):
     url = "https://www.gutenberg.org/files/100/100-0.txt"
     wget.download(url, "./shakespeare.txt")
     
-if not os.path.exists("./shakespeare300.txt"):
+n = 300
+if not os.path.exists(f"./shakespeare{n}.txt"):
     # Create empty file
-    open("./shakespeare300.txt", 'a').close()
+    open(f"./shakespeare{n}.txt", 'a').close()
     # Get lower content
     with open("./shakespeare.txt", "r+b") as fo:
         fo.seek(0)
         content = fo.read().lower()
     # Repeat 300 times
-    with open("./shakespeare300.txt", "r+b") as f:
-        for i in range(300):
+    with open(f"./shakespeare{n}.txt", "r+b") as f:
+        for i in range(n):
             f.write(content)
